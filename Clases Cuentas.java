7890feb44b0c5e179efcdb50f171da5c122835c4
EmiJoaquin <emij_novi19@gmail.com>
2025-07-09 00:29:36 -0300

    Se completan implementacion de clases

--- Clases Cuentas.java
+++ Clases Cuentas.java
@@ -2,4 +2,47 @@ class CuentaBancaria
 {
     //Atributos
     private int saldoPesos;
+    //Constructor
+    public CuentaBancaria()
+    {
+        this.saldoPesos = 0;
+    }
+    //Comandos
+    public void extraer(int monto)
+    {
+        this.saldoPesos -= monto;
+    }
+    public void depositar(int monto)
+    {
+        this.saldoPesos += monto;
+    }
+    //Consultas
+    public int consultarSaldo()
+    {
+        return this.saldoPesos;
+    }
+}
+class CuentaEspecial extends CuentaBancaria
+{
+    //Atributos
+    private int saldoUSD;
+    //Constructor
+    public CuentaEspecial()
+    {
+        this.saldoUSD = 0;
+    }
+    //Comandos
+    public void extraerUSD(int monto)
+    {
+        this.saldoUSD -= monto;
+    }
+    public void depositarUSD(int monto)
+    {
+        this.saldoUSD += monto;
+    }
+    //Consultas
+    public int consultarSaldo()
+    {
+        return this.saldoUSD;
+    }
 }
