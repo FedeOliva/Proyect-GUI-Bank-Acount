10e0d078d70311ce7053325d8486a43c0a01c34a
EmiJoaquin <emij_novi19@gmail.com>
2025-07-11 19:56:23 -0300

    Set class MiBoton

---
+++ Clase Boton.java
@@ -0,0 +1,14 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JButton;
+
+class MiBoton extends JButton
+{
+    public MiBoton (String text)
+    {
+        super(text);
+        setPreferredSize(new Dimension((int) getPreferredSize().getWidth()+1, 15));
+        setBackground(new Color(200, 200, 200));
+        setForeground(new Color(0, 0, 0));
+    }
+}
